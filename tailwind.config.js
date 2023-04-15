2568251fd01878c748f2729199664a35ba821bf0
Kehinde Malagu <Kehindemalagu@gmail.com>
2023-04-15 19:48:13 +0100

    Setup

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors : {
+        'red': '#f95b5b',
+        'plaster': '#e5dede'
+      }
+    },
+  },
+  plugins: [],
+}
+
